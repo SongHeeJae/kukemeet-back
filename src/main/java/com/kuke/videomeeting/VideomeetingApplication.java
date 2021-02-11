fcaa49b3537fe2077365332db0e372c00500e1cb
SongHeeJae <gmlwo308@gmail.com>
2021-02-11 16:08:08 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/kuke/videomeeting/VideomeetingApplication.java
@@ -0,0 +1,13 @@
+package com.kuke.videomeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideomeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideomeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuke/videomeeting/VideomeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuke.videomeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideomeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
