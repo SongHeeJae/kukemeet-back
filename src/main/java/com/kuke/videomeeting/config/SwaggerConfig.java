a8a66171c37920fd24b50fc24445cc32a921239e
SongHeeJae <gmlwo308@gmail.com>
2021-02-12 18:58:35 +0900

    feat: swagger api 문서 작성

---
+++ src/main/java/com/kuke/videomeeting/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.kuke.videomeeting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.kuke.videomeeting.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("Video Meeting API documentation")
+                .description("화상 회의 어플리케이션 API 서버 문서입니다.")
+                .license("gmlwo308@gmail.com")
+                .licenseUrl("https://github.com/songheejae/videomeeting-back").version("1").build();
+    }
+}
--- src/main/java/com/kuke/videomeeting/controller/sign/SignController.java
+++ src/main/java/com/kuke/videomeeting/controller/sign/SignController.java
@@ -4,10 +4,13 @@ import com.kuke.videomeeting.model.dto.response.Result;
 import com.kuke.videomeeting.model.dto.user.UserRegisterRequestDto;
 import com.kuke.videomeeting.service.common.ResponseService;
 import com.kuke.videomeeting.service.sign.SignService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 
+@Api(value = "Sign Controller", tags = {"Sign"})
 @RestController
 @RequestMapping("/api/sign")
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class SignController {
     private final ResponseService responseService;
     private final SignService signService;
 
+    @ApiOperation(value="회원가입", notes = "회원가입을 한다.")
     @PostMapping(value = "/register")
     public Result register(@RequestBody UserRegisterRequestDto requestDto) {
         signService.register(requestDto);
