a08e69e790284178720569fe1324a0efac7d4998
SongHeeJae <gmlwo308@gmail.com>
2021-03-07 03:09:59 +0900

    fix: security exception redirect https도 가능하게 수정

--- src/main/java/com/kuke/videomeeting/config/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/kuke/videomeeting/config/security/CustomAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.kuke.videomeeting.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -9,8 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.sendRedirect("/exception/accessdenied");
+        response.sendRedirect(request.getScheme().equals("https")
+                ? "https://api.kukemeet.com/exception/accessdenied" : "/exception/accessdenied");
     }
 }
--- src/main/java/com/kuke/videomeeting/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/kuke/videomeeting/config/security/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.kuke.videomeeting.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -9,8 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendRedirect("/exception/entrypoint");
+        response.sendRedirect(request.getScheme().equals("https")
+                ? "https://api.kukemeet.com/exception/entrypoint" : "/exception/entrypoint");
     }
+
 }
